1b82ae1d5363a2f0e068a851ebb90f70677da5f1
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2025-06-25 14:14:49 +0530

    feat: Add bun.lock file and update package dependencies

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from '@vitejs/plugin-react';
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import "./src/lib/env/server";
@@ -9,6 +10,7 @@ export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tailwindcss(),
+    react(),
     tanstackStart({
       target: "vercel",
     }),
